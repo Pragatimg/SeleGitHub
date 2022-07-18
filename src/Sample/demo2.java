bb7343c58d2766b52f88012d8ece108eafd60d34
qqqqqq <qqqqqq@DESKTOP-M5KOCQ7>
2022-07-18 20:37:36 +0530

    [pragati] added new demo2 class

---
+++ src/Sample/demo2.java
@@ -0,0 +1,18 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class demo2 
+{
+ public static void main(String[] args) {
+	
+	 Scanner scan=new Scanner(System.in);
+	 
+	 System.out.println("entre number:");
+	 
+	 int num1=scan.nextInt();
+	 
+	 System.out.println(num1);
+	 
+}
+}
